8ca52b258a6d947b9885800ff2817598a35ea912
The One Reborn <mortonmarrion@gmail.com>
2024-11-29 22:02:53 +0300

    feat: pass the retrieved query along with the prompt template to chat model and get a response

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from langchain.document_loaders import DirectoryLoader
+import os
+import argparse
+
+from dotenv import load_dotenv, find_dotenv
+
+from scripts.load_query_from_chroma import load_query_from_chroma
+from scripts.get_prompt_template import get_prompt_template
+from scripts.pass_prompt_template_to_model import pass_prompt_template_to_model
 
 
-DATA_PATH = "data"
+def main():
+    load_dotenv(find_dotenv())
+
+    chroma_path = os.getenv("CHROMA_PATH")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('query_text', type=str, help='Query text')
+    args = parser.parse_args()
+    query_text = args.query_text
+
+    result = load_query_from_chroma(chroma_path, query_text)
+
+    context_text = '\n\n---\n\n'.join([
+        document.page_content for document, _score in result
+    ])
+    sources = [
+        document.metadata.get('source', None) for document, _score in result
+    ]
+
+    model_response = pass_prompt_template_to_model(context_text, query_text, sources)
+
+    print(model_response)
 
 
-def load_documents():
-    loader = DirectoryLoader(DATA_PATH, glob="*.pdf")
-    documents = loader.load()
-
-    return documents
-
-
+if __name__ == "__main__":
+    main()
---
+++ scripts/pass_prompt_template_to_model.py
@@ -0,0 +1,16 @@
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+
+from scripts.get_prompt_template import get_prompt_template
+
+
+def pass_prompt_template_to_model(context_text, query_text, sources):
+    PROMT_TEMPLATE = get_prompt_template()
+    prompt_template = ChatPromptTemplate.from_template(PROMT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, query=query_text)
+
+    model = ChatOllama(model='llama3')
+    
+    response_text = model.invoke(prompt)
+
+    return response_text
