3210825286a960b9d3bfa952a3c71412838e2d31
The One Reborn <mortonmarrion@gmail.com>
2024-11-29 23:24:31 +0300

    fix: remove deprecated installations and change models in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,13 @@
 echo "Installing ollama software..."
 curl -fsSL https://ollama.com/install.sh | sh
 
-echo "Pulling gemma2 model..."
-ollama pull gemma2
+echo "Pulling llama3 model..."
+ollama pull llama3
 
 echo "Pulling nomic-embed-text..."
 ollama pull nomic-embed-text
 
 echo "Making sure ollama is served..."
 ollama serve
-
-echo "Creating a model from Modelfile..."
-ollama create cryptopaychatbot -f ./Modelfile
-
-npm install -g pnpm
 
 echo "Setup completed."
