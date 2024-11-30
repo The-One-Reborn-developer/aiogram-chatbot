ee312cb91b85cc4f637fe244e0ce4a0675a1c798
The One Reborn <mortonmarrion@gmail.com>
2024-11-30 12:02:41 +0300

    feat: bot startup

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 import os
-import argparse
+import asyncio
 
 from dotenv import load_dotenv, find_dotenv
 
-from app.scripts.load_query_from_chroma import load_query_from_chroma
-from app.scripts.pass_prompt_template_to_model import pass_prompt_template_to_model
+from aiogram import Bot, Dispatcher
+
+from app.routes.start import start_router
+from app.routes.message import message_router
 
 
-def main():
+async def main():
     load_dotenv(find_dotenv())
 
-    chroma_path = os.getenv("CHROMA_PATH")
+    bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
+    dp = Dispatcher()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('query_text', type=str, help='Query text')
-    args = parser.parse_args()
-    query_text = args.query_text
+    dp.include_routers(
+        start_router,
+        message_router
+    )
 
-    result = load_query_from_chroma(chroma_path, query_text)
-
-    context_text = '\n\n---\n\n'.join([
-        document.page_content for document, _score in result
-    ])
-    sources = [
-        document.metadata.get('source', None) for document, _score in result
-    ]
-
-    model_response = pass_prompt_template_to_model(context_text, query_text, sources)
-
-    print(model_response)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
