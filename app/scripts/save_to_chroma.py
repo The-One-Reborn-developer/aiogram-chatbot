7a9320e6fbd9b7f00f5bfb496774e8e6b0f5f517
The One Reborn <mortonmarrion@gmail.com>
2024-12-01 21:35:21 +0300

    refactor: save only new chunks to database

--- app/scripts/save_to_chroma.py
+++ app/scripts/save_to_chroma.py
@@ -1,24 +1,37 @@
-import os
-import shutil
+import logging
 
 from langchain_chroma import Chroma
 
 from app.scripts.get_embedding_function import get_embedding_function
 
 
-def save_to_chroma(chunks, chroma_path) -> bool:
-    if os.path.exists(chroma_path):
-        shutil.rmtree(chroma_path)
-
+def save_to_chroma(chunks_with_ids, chroma_path) -> bool:
     try:
-        Chroma.from_documents(
-            chunks,
-            embedding=get_embedding_function(),
+        db = Chroma(
+            embedding_function=get_embedding_function(),
             persist_directory=chroma_path,
         )
-    except Exception as e:
-        print(e)
-        return False
 
-    print(f'Saved {len(chunks)} chunks to {chroma_path}')
-    return True
+        # Get existing document ids
+        existing_documents = db.get(include=[])
+        existing_ids = set(existing_documents['ids'])
+        logging.info(f'Number of existing documents in Chroma: {len(existing_ids)}')
+    
+        # Filter out duplicate chunks
+        new_chunks = [chunk for chunk in chunks_with_ids if chunk.metadata['id'] not in existing_ids]
+
+        if not new_chunks:
+            logging.info('No new chunks to save to Chroma')
+            return True
+
+        # Only add new chunks
+        new_chunks_ids = [chunk.metadata['id'] for chunk in new_chunks]
+
+        logging.info(f'Saving {len(new_chunks)} new documents to Chroma')
+        db.add_documents(new_chunks, ids=new_chunks_ids)
+
+        logging.info(f'Saved {len(new_chunks)} documents to Chroma')
+        return True
+    except Exception as e:
+        logging.error(e)
+        return False
--- populate_database.py
+++ populate_database.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from dotenv import load_dotenv, find_dotenv
 
@@ -9,34 +10,37 @@ from app.scripts.save_to_chroma import save_to_chroma
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    logging.info("Loading environment variables")
     load_dotenv(find_dotenv())
 
+    logging.info("Loading data")
     documents = load_documents(os.getenv("DATA_PATH"))
     
-    
-    chunks = split_documents(documents)
+    logging.info("Splitting documents")
+    chunks_with_ids = split_documents(documents)
 
     last_page_id = None
     current_chunk_index = 0
 
-    for chunk in chunks:
-        source = chunk.metadata.get('source', None)
-        page = chunk.metadata.get('page', None)
-        current_page_id = f'{source}:{page}'
-
-        if current_page_id == last_page_id:
-            current_chunk_index += 1
-        else:
-            current_chunk_index = 0
-
-        chunk.metadata['id'] = current_page_id
+    logging.info("Adding metadata to chunks")
+    for index, chunk in enumerate(chunks_with_ids):
+        source = chunk.metadata.get('source', 'unknown_source')
+        page = chunk.metadata.get('page', -1)
+        chunk.metadata['id'] = f'{source}:{page}:{index}'
     
+    logging.info("Saving to chroma")
     chroma_path = os.getenv("CHROMA_PATH")
     
-    result = save_to_chroma(chunks, chroma_path)
+    result = save_to_chroma(chunks_with_ids, chroma_path)
 
     if not result:
-        print('Failed to save to chroma')
+        logging.error("Failed to save to chroma")
         exit(1)
 
     exit(0)
